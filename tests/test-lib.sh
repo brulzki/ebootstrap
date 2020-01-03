a548b9e5a6b9b9254da908f004fbd117f4d32e59
Bruce Schultz <brulzki@gmail.com>
2020-01-03 10:09:28 +1000

    tests: Converts test output to TAP format

--- tests/test-lib.sh
+++ tests/test-lib.sh
@@ -22,8 +22,20 @@ inherit() {
 }
 EXPORT_FUNCTIONS() { :; }
 
+# test stubs
+ebootstrap-rc-update() { :; }
+
+#
+# test functions
+#
+test_count=0
+test_failure=0
+test_success=0
+test_case=""
+
 tbegin () {
-    echo "$@" >&2
+    test_case="$@"
+    printf "# %s\n" "${test_case}"
     __is_fn setup && setup
 }
 
@@ -35,19 +47,45 @@ is_debug() {
     [[ $V == 1 ]]
 }
 
+test_ok_() {
+    (( test_success++ ))
+    printf "ok %s - %s\n" "${test_count}" "$1"
+}
+
+test_failure_() {
+    (( test_failure++ ))
+    printf "not ok %s - %s\n" "${test_count}" "$1"
+    shift
+    printf '%s\n' "$*" | sed -e 's/^/#	/'
+}
+
 assert() {
     local t="$1"; shift
-    eval "$@"
+    (( test_count++ ))
+    eval "$@" </dev/null >&3 2>&4
     local ret=$?
     if [[ $ret == 0 ]]; then
-        echo "PASSED: $t" >&2
+        test_ok_ "$t"
     else
-        echo "FAILED: $t" >&2
+        test_failure_ "$t" "$@"
     fi
     return $ret
 }
 
-# stub
-ebootstrap-rc-update() {
-    :
+test_summary() {
+    cat <<-EOF
+	1..${test_count}
+	# total   ${test_count}
+	# success ${test_success}
+	# failed  ${test_failure}
+	EOF
 }
+
+trap 'test_summary' EXIT
+
+if is_debug; then
+    # set up verbose output redirection
+    exec 3>&1 4>&2
+else
+    exec 3>/dev/null 4>/dev/null
+fi
